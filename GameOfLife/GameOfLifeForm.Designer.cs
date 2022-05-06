bbb375c4caf4fc6d3b253aa8c9234deb30746e24
Andrew Galbraith <andrew.galbraith@jonkoping.se>
2022-05-06 10:29:04 +0200

    Initial commit

---
+++ GameOfLife/GameOfLifeForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace GameOfLife
+{
+    partial class GameOfLifeForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            
+         
+            // 
+            // GameOfLifeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "GameOfLifeForm";
+            this.Text = "Game Of Life";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.GameOfLifeForm_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
---
+++ GameOfLife/GameOfLifeForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public partial class GameOfLifeForm : Form
+    {
+        public GameOfLifeForm()
+        {
+            InitializeComponent();
+            gameTimer.Interval = 1000;
+            gameTimer.Start();
+        }
+
+        //Time code here..what should happen every time interval?
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //Don't forget to call Invalidate() to update the display - so GameOfLifeForm_Paint is called...
+            Invalidate();
+        }
+
+        //Painting code here...
+        private void GameOfLifeForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            SolidBrush b = new SolidBrush(Color.Green);   
+        }
+
+    }
+}
